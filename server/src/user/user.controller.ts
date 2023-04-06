092a697edb3ba65d27f4476a30e906829e21f9c6
Rohit Saini <rohit@prodios.com>
2023-04-06 21:19:03 +0530

    feat(users): add me route to get logged in user

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -2,10 +2,17 @@ import { Controller, Get, UseGuards } from '@nestjs/common'
 import { UserService } from './user.service'
 import { UserWithoutSensitiveData } from './user.type'
 import { JwtGuard } from '~/auth/jwt/jwt.guard'
+import { GetUser } from '~/auth/user.decorator'
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @UseGuards(JwtGuard)
+  @Get('me')
+  findMe(@GetUser() user: UserWithoutSensitiveData): UserWithoutSensitiveData {
+    return user
+  }
 
   @UseGuards(JwtGuard)
   @Get()
