be7d4cdc5e78236d08e0973df9a0e1173e27ea22
Rohit Saini <rohit@prodios.com>
2023-05-21 12:50:49 +0530

    feat(home): add Create New Post button on homepage

--- client/src/pages/home/home.tsx
+++ client/src/pages/home/home.tsx
@@ -1,4 +1,5 @@
 import { range } from 'lodash'
+import { Link } from 'react-router-dom'
 import AppShell from '~/components/app-shell'
 import Page from '~/components/page'
 import Post from '~/components/post'
@@ -12,6 +13,12 @@ export default function Home() {
     >
       <div className="col-span-3 hidden lg:block" />
       <div className="col-span-full space-y-4 border-l-2 border-r-2 p-4 lg:col-span-6">
+        <div className="mb-4 flex items-center justify-center rounded-2xl border-2 p-4">
+          <Link to="/create-new-post" className="bg-primary text-background rounded-full px-4 py-2 shadow">
+            Create New Post
+          </Link>
+        </div>
+
         {range(5).map((_, idx) => (
           <Post key={idx} />
         ))}
