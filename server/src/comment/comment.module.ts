e03339ce76a7b45b0d3f5e68da60f533f68409c0
Rohit Saini <rohit@prodios.com>
2023-06-19 21:51:42 +0530

    feat(notification): creating notification while commenting on a post

--- server/src/comment/comment.module.ts
+++ server/src/comment/comment.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common'
 import { CommentController } from './comment.controller'
 import { CommentService } from './comment.service'
 import { PrismaService } from '~/prisma/prisma.service'
+import { NotificationModule } from '~/notification/notification.module'
+import { PostModule } from '~/post/post.module'
 
 @Module({
-  imports: [],
+  imports: [PostModule, NotificationModule],
   controllers: [CommentController],
   providers: [CommentService, PrismaService],
   exports: [CommentService],
--- server/src/comment/comment.service.ts
+++ server/src/comment/comment.service.ts
@@ -4,20 +4,33 @@ import { PrismaService } from '~/prisma/prisma.service'
 import { CreateCommentDto } from './comment.dto'
 import { UserWithoutSensitiveData } from '~/user/user.type'
 import { COMMENT_INCLUDE_FIELDS } from './comment.fields'
+import { NotificationService } from '~/notification/notification.service'
+import { PostService } from '~/post/post.service'
 
 @Injectable()
 export class CommentService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly postService: PostService,
+    private readonly notificationService: NotificationService,
+  ) {}
 
-  createComment(postId: string, dto: CreateCommentDto, user: UserWithoutSensitiveData): Promise<Comment> {
-    return this.prismaService.comment.create({
-      data: {
-        commentById: user.id,
-        postId,
-        comment: dto.comment,
-      },
-      include: COMMENT_INCLUDE_FIELDS,
-    })
+  async createComment(postId: string, dto: CreateCommentDto, user: UserWithoutSensitiveData): Promise<Comment> {
+    const post = await this.postService.findOneById(postId)
+
+    const [commentCreated] = await Promise.all([
+      this.prismaService.comment.create({
+        data: {
+          commentById: user.id,
+          postId,
+          comment: dto.comment,
+        },
+        include: COMMENT_INCLUDE_FIELDS,
+      }),
+      this.notificationService.createNotification(user.id, post.createdById, 'commented on your post.'),
+    ])
+
+    return commentCreated
   }
 
   deleteComment(id: string): Promise<Comment> {
